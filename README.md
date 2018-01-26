058a7d80463f8bc3a4dd25b76cd3f08a816b328d
Rohit <rohitgpt@outlook.com>
2018-01-26 17:46:31 +0530

    Final Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Color Game Project
+
+This website is hosted: 
+https://rgbcolourgame.herokuapp.com/
+
+An RGB coded number will be provided at the top and you need to predict actual colour from one of the palettes. Game has two levels: Easy and Hard.
---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function(req,res){
+	res.render("landing");
+});
+
+app.listen(process.env.PORT,process.env.IP, function() {
+	console.log("Server started at 8080");
+});
